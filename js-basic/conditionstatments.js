e0b7d3a82ddedad76563509f41e28200f5db83d2
Mital-idea2code <mk.idea2code@gmail.com>
2021-02-20 18:17:58 +0530

    javascript prectices

---
+++ js-basic/conditionstatments.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// let age  = parseInt(prompt("Enter your age:"));
+// alert(`You are ${age} year old.`);
+// console.log(typeof(age));
+
+// if (age >= 10){
+//     alert("You are able");
+// }else{
+//     alert("You are not able for this.");
+// }
+
+// let WinningNumber = 8;
+// let Numbers = parseInt(prompt("Enter you Luck number"));
+
+// if(Numbers > WinningNumber)
+// {
+//     alert("Too Big number enter");
+// }
+// else if(Numbers < WinningNumber)
+// {
+//     alert("Too samll number enter ");
+// }
+// else
+// {
+//     alert("WoW! You Enter right number");
+// }
---
+++ js-basic/index.js
@@ -0,0 +1,50 @@
+'use strcit'
+
+// Data types
+
+// console.log('Hello world');
+// let name = "Idea2code";
+// const age = 20;
+// console.log(age);
+// console.log(name);
+
+//  object
+
+// let person = {
+//     name:'hardik',
+//     age:20
+// };
+// person.name = 'Idea2Code';
+// console.log(person);    
+
+// Array
+
+// var selectcolor = ['Red','Yellow','Blue','black'];
+// selectcolor[0] = 'Pink';
+// console.log(selectcolor);
+
+// let firstName = "Hardik";
+// let lastName = "Mandaviya";
+// let fullName = `Hardik Mandaviya`
+// let companyName = "Idea2code";
+// console.log(firstName);
+
+// Template string concatenation
+// let FullName = `I am ${fullName} and working on ${companyName}.`
+
+// Simple concatenation
+// let FullName = "I am " +fullName + "and working on " +companyName + "."
+// let newStr = companyName.slice(0,4)
+// console.log(newStr);
+// let num = 20.20;
+// let str = "11";
+// console.log(num + parseInt(str));
+// let minNum = 5;
+// let maxNum = 10;
+// let rendomNum = Math.floor(Math.random() * (maxNum - minNum) + minNum); //5 + minNum
+// console.log(rendomNum);
+// let number = 10;
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// let number =  9007199254740991n;
+// console.log(number + 109n);
